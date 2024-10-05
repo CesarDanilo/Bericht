ea8001746d55842b363767047527fec343a9e576
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-05 14:29:57 -0300

    CREATED PAGE HOME

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './views/home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home />
   )
 }
 
---
+++ FrontEnd/src/views/home/index.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div className='bg-custom-black h-screen'>
+        </div>
+    )
+}
+
+export default Home;
