3ba855ffb952e9d6feaf7bdc8fc3f2a13bd3ad4a
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-05 14:38:09 -0300

    CREATED COMPONENTS

--- FrontEnd/src/components/Main/index.jsx
+++ FrontEnd/src/components/Main/index.jsx
@@ -1,7 +1,11 @@
 const Main = () => {
     return (
-        <>
-        </>
+        <div>
+            {/* div componente da esquerda */}
+            <div></div>
+            {/* div componente da direita */}
+            <div></div>
+        </div>
     )
 }
 
