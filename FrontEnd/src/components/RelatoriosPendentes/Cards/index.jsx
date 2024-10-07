ebbf967f7f1e002056a1906c1f336810a44b0cc5
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-07 14:39:04 -0300

    Update index.jsx

--- FrontEnd/src/components/RelatoriosPendentes/Cards/index.jsx
+++ FrontEnd/src/components/RelatoriosPendentes/Cards/index.jsx
@@ -1,10 +1,10 @@
 const Cards = () => {
     return (
-        <div className=" bg-custom-black w-full rounded-lg">
-            <div>
-                <img src="" alt="" />
+        <div className="flex align-middle p-2 bg-custom-black w-full rounded-lg">
+            <div className=" w-14 h-14 bg-white m-4 rounded-full">
+                <img className="w-full" src="" alt="" />
             </div>
-            <div className="flex-col p-4 ">
+            <div className="flex-col p-3 w-9/12">
                 <h1 className="text-neutral-200 font-semibold">Nome da empresa aqui</h1>
                 <p className="text-neutral-500 font-light text-sm">Descrição do relatorio</p>
                 <div className="flex justify-between">
