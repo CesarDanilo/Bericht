d1b108c2b83fc064843c388c03cc17f0e50653ff
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-14 14:43:56 -0300

    Update index.jsx

--- FrontEnd/src/components/Main/index.jsx
+++ FrontEnd/src/components/Main/index.jsx
@@ -101,9 +101,12 @@ const Main = () => {
                 {/* Histórico */}
                 <div className="flex flex-col bg-[#191919] rounded-lg p-4 space-y-4 md:w-[30%] max-h-[500px] overflow-y-auto">
                     <h3 className="text-white font-bold text-lg">Histórico</h3>
-                    <div>
-                        <button className="text-primary" onClick={() => { handleDeleteHistoryRelatorios() }}>Limpar Historico</button>
-                    </div>
+                    {
+                        historicoRelatorios.length > 0 &&
+                        <div>
+                            <button className="text-primary" onClick={() => { handleDeleteHistoryRelatorios() }}>Limpar Historico</button>
+                        </div>
+                    }
                     {historicoRelatorios.map((item) => (
                         <div key={item.id}>
                             <Cards
