99cf92af3265357bf992fe88a22a7fcc595c8889
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-16 20:32:25 -0300

    Update index.jsx

--- FrontEnd/src/components/Main/index.jsx
+++ FrontEnd/src/components/Main/index.jsx
@@ -61,6 +61,20 @@ const Main = () => {
     const handleDelete = async () => {
         // Atualiza o histórico de relatórios
         setHistoricoRelatorios([...historicoRelatorios, selectedCard]); // Adiciona o item selecionado ao histórico
+        // salvarRelatoriosConcluidos([historicoRelatorios]);
+        // await salvarHistoricoRelatorios(); // Chama a função para salvar o histórico
+
+        // Atualiza os relatórios
+        const updatedRelatorios = relatorios.filter((item) => item.id !== selectedCard.id);
+        setRelatorios(updatedRelatorios);
+        localStorage.setItem("relatorios", JSON.stringify(updatedRelatorios));
+        setIsDialogOpen(false);
+        setSelectedCard(null);
+    };
+
+    const handleDeleteConcluir = async () => {
+        // Atualiza o histórico de relatórios
+        setHistoricoRelatorios([...historicoRelatorios, selectedCard]); // Adiciona o item selecionado ao histórico
         salvarRelatoriosConcluidos([historicoRelatorios]);
         await salvarHistoricoRelatorios(); // Chama a função para salvar o histórico
 
@@ -124,143 +138,6 @@ const Main = () => {
     }, [evento, relatorios]); // Adiciona relatorios ao array de dependências
 
     return (
-        // <div className="flex flex-col lg:flex-row justify-center items-start min-h-screen p-10">
-        //     {/* Container Principal */}
-        //     <div className="flex flex-col md:flex-col w-full gap-4">
-        //         {/* Componente Esquerdo */}
-        //         <div className='flex w-full gap-4'>
-        //             <div className="bg-[#191919] rounded-lg w-full md:w-[60%] h-96 p-6 shadow-md">
-        //                 {/* Conteúdo dentro do componente esquerdo */}
-        //             </div>
-        //             {/* Histórico */}
-        //             {
-        //                 historicoRelatorios.length > 0 && (
-        //                     <div className="flex flex-col bg-[#191919] rounded-lg p-4 space-y-4 md:w-[20%] max-h-[384px] overflow-y-auto scrollbar-hidden">
-        //                         <h3 className="text-white font-bold text-lg">Histórico</h3>
-
-        //                         <div>
-        //                             <button className="text-primary" onClick={() => { handleDeleteHistoryRelatorios() }}>
-        //                                 Limpar Histórico
-        //                             </button>
-        //                         </div>
-
-        //                         {historicoRelatorios.map((item) => (
-        //                             <div key={item.id}>
-        //                                 <Cards
-        //                                     empresa={item.empresa}
-        //                                     dataFinal={item.dataFinal}
-        //                                 />
-        //                             </div>
-        //                         ))}
-        //                     </div>
-        //                 )
-        //             }
-        //         </div>
-        //         <div className="flex rounded-lg w-full md:w-[60%] gap-4">
-        //             <CardsGadgets descricao={"Relatorios pendente"} valor={relatorios.length} />
-        //             <CardsGadgets descricao={"Relatorios atrazados"} valor={relatoriosAtrazados} />
-        //         </div>
-        //         <div className="flex rounded-lg w-full md:w-[60%] gap-4">
-        //             <CardsGadgets descricao={"Total de Relatorios"} valor={relatorios.length + relatoriosConcluidos.length} />
-        //             <CardsGadgets descricao={"Relatorios Concluidos"} valor={relatoriosConcluidos.length} />
-        //         </div>
-        //     </div>
-
-        //     {/* Relatórios Pendentes */}
-        //     <div className="bg-[#191919] rounded-lg w-full md:w-[680px] h-96 p-6 shadow-md">
-        //         <div className="flex justify-between items-center scrollbar-hidden">
-        //             <h3 className="text-white font-bold text-lg">Relatórios Pendentes</h3>
-        //             <button
-        //                 className="flex justify-center items-center rounded-full bg-primary w-8 h-8"
-        //                 onClick={() => setIsDialogOpenAddRelatorio(true)}
-        //             >
-        //                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-black">
-        //                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-        //                 </svg>
-        //             </button>
-        //         </div>
-        //         <div className="flex flex-col space-y-4 mt-4 max-h-[384px] h-72 overflow-y-auto scrollbar-hidden">
-        //             {relatorios.map((item) => (
-        //                 <div key={item.id} onClick={() => handleCardClick(item)}>
-        //                     <Cards
-        //                         empresa={item.empresa}
-        //                         descricao={item.descricao}
-        //                         dataInicial={item.dataInicial}
-        //                         dataFinal={item.dataFinal}
-        //                     />
-        //                 </div>
-        //             ))}
-        //         </div>
-        //     </div>
-
-        //     {/* Modal para cadastro de novo relatório */}
-        //     {isDialogOpenAddRelatorio && (
-        //         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        //             <div className="bg-[#191919] rounded-lg p-6 w-3/4 shadow-lg">
-        //                 <h3 className="text-white font-bold text-xl mb-4">Cadastrar Novo Relatório</h3>
-        //                 <form onSubmit={handleSubmit}>
-        //                     <input
-        //                         type="text"
-        //                         name="empresa"
-        //                         placeholder="Nome da Empresa"
-        //                         value={empresa}
-        //                         onChange={(e) => setEmpresa(e.target.value)}
-        //                         className="w-full p-2 mb-2 rounded bg-custom-black text-neutral-200"
-        //                         required
-        //                     />
-        //                     <textarea
-        //                         name="descricao"
-        //                         placeholder="Descrição"
-        //                         value={descricao}
-        //                         onChange={(e) => setDescricao(e.target.value)}
-        //                         className="w-full p-2 mb-2 rounded bg-custom-black text-neutral-200"
-        //                         required
-        //                     />
-        //                     <input
-        //                         type="date"
-        //                         name="dataInicial"
-        //                         value={dataInicial}
-        //                         onChange={(e) => setDataInicial(e.target.value)}
-        //                         className="w-full p-2 mb-2 rounded bg-custom-black text-neutral-200"
-        //                         required
-        //                     />
-        //                     <input
-        //                         type="date"
-        //                         name="dataFinal"
-        //                         value={dataFinal}
-        //                         onChange={(e) => setDataFinal(e.target.value)}
-        //                         className="w-full p-2 mb-2 rounded bg-custom-black text-neutral-200"
-        //                         required
-        //                     />
-        //                     <div className="flex justify-between mt-4">
-        //                         <button type="button" className="bg-custom-black text-neutral-500 rounded px-4 py-2" onClick={() => setIsDialogOpenAddRelatorio(false)}>Cancelar</button>
-        //                         <button type="submit" className="bg-primary text-neutral-900 rounded px-4 py-2">Cadastrar</button>
-        //                     </div>
-        //                 </form>
-        //             </div>
-        //         </div>
-        //     )}
-
-        //     {/* Modal para exibir detalhes do card selecionado */}
-        //     {isDialogOpen && selectedCard && (
-        //         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        //             <div className="bg-[#191919] rounded-lg p-6 w-full max-w-[768px] shadow-lg">
-        //                 {/* Conteúdo do modal */}
-        //                 <h1 className='text-neutral-200 font-semibold mt-3'>Empresa: {selectedCard.empresa}</h1>
-        //                 <h2 className='text-neutral-200 font-semibold mt-3'>Descrição: {selectedCard.descricao}</h2>
-        //                 <div className="flex gap-52">
-        //                     <h2 className='text-neutral-200 font-semibold mt-3'>Data Inicial: {selectedCard.dataInicial}</h2>
-        //                     <h2 className='text-primary font-semibold mt-3 italic'>Data Final: {selectedCard.dataFinal}</h2>
-        //                 </div>
-        //                 <div className='flex gap-80'>
-        //                     <button className='flex align-middle justify-center rounded-lg p-0  mt-3 bg-red-600 w-16 h-7 text-neutral-200' onClick={handleDelete}>Excluir</button>
-        //                     <button className='flex align-middle justify-center rounded-lg p-0  mt-3 bg-primary w-16 h-7' onClick={() => setIsDialogOpen(false)}>Sair</button>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     )}
-        // </div>
-
         <div className="flex flex-col lg:flex-row justify-center items-start min-h-screen p-10">
             {/* Container Principal */}
             <div className="flex flex-col w-full gap-4 p-4">
@@ -400,7 +277,7 @@ const Main = () => {
                             <button className="bg-red-500 rounded-lg px-4 py-2 text-neutral-200" onClick={handleDelete}>Excluir</button> {/* Aumentei o tamanho dos botões */}
                             <div className="flex space-x-2"> {/* Criei um contêiner para os dois botões */}
                                 <button className="bg-primary rounded-lg px-4 py-2 text-black" onClick={() => setIsDialogOpen(false)}>Sair</button> {/* Botão Sair */}
-                                <button className="bg-green-600 rounded-lg px-4 py-2 text-neutral-200">Concluir</button> {/* Botão Concluir */}
+                                <button className="bg-green-600 rounded-lg px-4 py-2 text-neutral-200" onClick={() => { handleDeleteConcluir() }}>Concluir</button> {/* Botão Concluir */}
                             </div>
                         </div>
                     </div>
